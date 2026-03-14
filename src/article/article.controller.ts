76a6b940b9cdfb661208dc861b75078d034f738a
purerosefallen <78877@qq.com>
2026-03-14 11:07:16 +0800

    fix

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,10 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { ArticleFactory } from './article.factory';
 import { ArticleService } from './article.service';
+import { ApiInject } from 'nicot';
 
 @Controller('article')
 export class ArticleController extends ArticleFactory.baseController() {
-  constructor(private service: ArticleService) {
+  constructor(@ApiInject() private service: ArticleService) {
     super(service);
   }
 }
